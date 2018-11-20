1763ddc381fe5eec3d60e0eda075eda245c73910
Keith Nguyen <khoacn@gmail.com>
2018-11-20 11:45:15 -0800

    implemented autoLogout Function JWT expire

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'goluckycat';
+
+  constructor(){
+  }
+
+  ngOnInit() {
+    this.isloggedIn()
+  }
+
+  isloggedIn(){
+    var token = localStorage.getItem('token')
+    if(!token){
+      localStorage.clear()
+      return false
+    }else{
+      var tokenPayload = decode(token)
+      var dateNow = new Date()
+      console.log('app:', tokenPayload.exp)
+      if(tokenPayload.exp < dateNow.getTime()/1000){
+        localStorage.clear()
+        return false
+      }
+    }
+        return true 
+  }
 }
